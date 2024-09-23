28097575ac5d77eeb4d013ee6d5773f401984cf6
MYWIND\117072 <dun.caiyun@relectric.cn>
2024-09-23 17:06:06 +0800

    6-10

--- src/10-ClassProp.js
+++ src/10-ClassProp.js
@@ -4,6 +4,10 @@ import { Component } from 'react'
 // 1. 父传子 直接通过prop子组件标签身上绑定父组件中的数据即可
 // 2. 子传父 在子组件标签身上绑定父组件中的函数，子组件中调用这个函数传递参数
 
+// 总结
+// 1. 思想保持一致
+// 2. 类组件依赖于this
+
 class Son extends Component {
  
   render() {
